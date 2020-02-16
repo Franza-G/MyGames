2c0ee097e3f4ba52c7d5a8ff73a64516bb105e61
Franza-G <FranzaGregoire@gmail.com>
2020-02-16 12:37:02 -0500

    Tutorial1.5

--- Tutorial1.5/Assets/Scripts/DogController.cs
+++ Tutorial1.5/Assets/Scripts/DogController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatController : MonoBehaviour
+public class DogController : MonoBehaviour
 {
     public AudioSource musicSource;
 
